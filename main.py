a2c75e3c40c790638cc9551aedb1abe09659d98a
Againnstt <wesleyyfrr@gmail.com>
2026-08-04 00:07:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,10 @@ def carregar_dados():
 df = carregar_dados()
 
     # cálculo usado no seu trabalho
-df["Variação da Nota"] = (df["Nota Depois do Semestre"]-df["Nota Antes do Semestre"])return df
+df["Variação da Nota"] = (df["Nota Depois do Semestre"]-df["Nota Antes do Semestre"])
+
+
+return df
 
 
 df = carregar_dados()
