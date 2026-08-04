852447953f9ea68df1dbf17e84e9d8802ffdbb14
Againnstt <wesleyyfrr@gmail.com>
2026-08-03 23:05:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+# -----------------------------
+# Configuração da página
+# -----------------------------
+st.set_page_config(
+    page_title="Impacto da IA",
+    page_icon="🤖",
+    layout="wide"
+)
