bdeea4a5b48a4d13954b76e41a3f3b67f7ce2294
Againnstt <wesleyyfrr@gmail.com>
2026-08-04 00:10:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,28 +27,19 @@ pode influenciar o desempenho acadêmico dos estudantes.""")
 
 @st.cache_data
 def carregar_dados():
-
     df = pd.read_csv("ai_student_impact_dataset.csv")
 
-    df = df.rename(columns={
-        "Student_ID": "ID Estudante",
-        "Major_Category": "Área do Curso",
-        "Year_of_Study": "Ano de Estudo",
-        "Pre_Semester_GPA": "Nota Antes do Semestre",
-        "Weekly_GenAI_Hours": "Horas Semanais Usando IA",
-        "Primary_Use_Case": "Principal Uso IA",
-        "Traditional_Study_Hours": "Horas de Estudo Tradicional",
-        "Post_Semester_GPA": "Nota Depois do Semestre",
-        "Burnout_Risk_Level": "Risco de Burnout"
-    })
+    df = df.rename(columns={"Student_ID":"ID Estudante",
+    "Major_Category":"Área do Curso",
+    "Year_of_Study":"Ano de Estudo",
+    "Pre_Semester_GPA":"Nota Antes do Semestre",
+    "Weekly_GenAI_Hours":"Horas Semanais Usando IA",
+    "Primary_Use_Case":"Principal Uso IA",
+    "Traditional_Study_Hours":"Horas de Estudo Tradicional",
+    "Post_Semester_GPA":"Nota Depois do Semestre",
+    "Burnout_Risk_Level":"Risco de Burnout"})
 
-
-    df["Variação da Nota"] = (
-        df["Nota Depois do Semestre"]
-        -
-        df["Nota Antes do Semestre"]
-    )
-
+    df["Variação da Nota"] = df["Nota Depois do Semestre"] - df["Nota Antes do Semestre"]
 
     return df
 
